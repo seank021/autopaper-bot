99ec499336beac0c4dbfb19df89ed6613827df2e
seank021 <seahnnnnnn@gmail.com>
2025-08-01 14:17:03 +0900

    data

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 INTEREST_DB = {
     "U090X2M5P8B": { # Sean
-        "keywords": ["education", "visulization", "social"],
+        "keywords": ["education", "visualization", "social"],
         "interests": "I'm interested in education area to improve the learning experience. I also like to work on visualization and social-problem-solving projects.",
+        "current_projects": "I'm working on graph based knowledge tracing, which is a project that aims to improve the learning experience by using graph-based methods to trace knowledge acquisition.",
     },
 }
