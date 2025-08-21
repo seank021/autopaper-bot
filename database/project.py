4cdc70d42720740fbeb60effe0fb551bf7ae968b
seank021 <seahnnnnnn@gmail.com>
2025-08-21 14:07:26 +0900

    fix: structure

--- database/project.py
+++ database/project.py
@@ -2,6 +2,7 @@
 
 PROJECT_DB = { # project channel slack id
     "C08TTLWA5EG": { # prj-collab-agent
+        "members": ["U090X2M5P8B", "", ""],
         "keywords": [],
         "description": "",
     }
